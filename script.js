c22a5bcf03cff42119af255ec93813760c59b767
amadeus-aleksei <105963162+amadeus-aleksei@users.noreply.github.com>
2022-07-10 00:09:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-var h3 = document.querySelector("h3");
+var h3First = document.getElementById("first");
+var h3Second = document.getElementById("second");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.querySelector("body");
 var button = document.querySelector("button");
 
-h3.textContent=color1.value + " " + color2.value;
+h3First.textContent=color1.value + " " + color2.value;
+h3Second.textContent="{ background:linear-gradient(to right, "
++color1.value+", "+color2.value+"); }";
 
 color1.addEventListener("input", function(){
     console.log(color1.value);
     body.style.background="linear-gradient(to right, "+color1.value
         +", "+color2.value+")";
-        h3.textContent=color1.value + " " + color2.value;
+    h3First.textContent=color1.value + " " + color2.value;
+    h3Second.textContent="{ background:linear-gradient(to right, "
+        +color1.value+", "+color2.value+"); }";
 })
 
 color2.addEventListener("input", function(){
     body.style.background='linear-gradient(to right, '+color1.value
         +", "+color2.value+")";
-        h3.textContent=color1.value + " " + color2.value;
+    h3First.textContent=color1.value + " " + color2.value;
+    h3Second.textContent="{ background:linear-gradient(to right, "
+        +color1.value+", "+color2.value+"); }";
 })
 
 button.addEventListener("click", function(event){
